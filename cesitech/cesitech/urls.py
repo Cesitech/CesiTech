80aacddca76d752b6391bfcff13e651677a25e0b
Nicolas MAZARD <nicolasmazard@gmail.com>
2016-02-08 22:03:15 +0100

    Add app admin

--- cesitech/cesitech/urls.py
+++ cesitech/cesitech/urls.py
@@ -6,5 +6,5 @@ from admin import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cesitech/', include('blog.urls')),
-    url(r'^admin/',include('blog.urls')),
+    url(r'^adminCesitech/',include('admin.urls')),
 ]
