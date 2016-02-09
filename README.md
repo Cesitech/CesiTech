afaba4c8549fe8b58e07a34993468f0a893cc391
nicolas MAZARD <nicolasmazard@gmail.com>
2016-02-09 08:23:30 +0100

    first rangement

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CesiTech
+sudo apt-get install libjpeg-dev
+sudo apt-get install zlib1g-dev 
+sudo apt-get install python-dev 
--- cesitech/adminCesitech/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from datetime import datetime
-
-# Create your views here.
-def index(request):
-    return render(request, 'admin/index.html', {'date':datetime.now()})
